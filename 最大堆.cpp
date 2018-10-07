2431845917f907d3dd1ff4e2c72a9f12063b1fef
liuyuxinzju <21725152@zju.edu.cn>
2018-10-07 21:48:30 +0800

    完成最大堆构建

--- 最大堆.cpp
+++ 最大堆.cpp
@@ -18,7 +18,7 @@ ElementType DeleteMax(MaxHeap H);
 
 MaxHeap Create(int MaxSize){
 	MaxHeap H = (MaxHeap)malloc(sizeof(struct HeapStruct));
-	H->Element = malloc(sizeof(ElementType)*(MaxSize+1));
+	H->Element = (int*)malloc(sizeof(ElementType)*(MaxSize+1));
 	H->HeapSize = 0;
 	H->Capacity = MaxSize;
 	H->Element[0] = -1;//小于堆中所有元素的值
@@ -42,6 +42,21 @@ ElementType Delete(MaxHeap H){
 	/*思路：1.删除并保存根节点，把最后一位提到根节点位置；
 	2.根节点与左右儿子比较，和较大儿子进行交换；
 	3.循环至不能交换，将根节点赋值给该位置*/
+	if(H->HeapSize==0){
+		cout<<"堆是空的"<<endl;
+		return -1; 
+	}
+	int max = H->Element[1];/*保存最大值*/
+	int temp = H->Element[H->HeapSize--];/*存好最后一位并减小堆size*//*1*/
+	int parent, child;
+	for(parent = 1;parent*2<=H->HeapSize;parent=child){/*条件：有子节点*/ 
+		child = parent*2;
+		if(child<=H->HeapSize && H->Element[child]<H->Element[child+1]) child++;/*条件：有子节点且右子节点更大*/
+		if(H->Element[child]<=temp) break;/*更大的子节点小于最后一位节点的话，退出*/
+		else H->Element[parent]=H->Element[child]; /*更大的子节点大于最后一位节点的话，交换*//*2*/ 
+	} 
+	H->Element[parent]=temp;//上面的所有交换其实都是赋值，最后是3步交换的第三步 /*3*/
+	return max;
 } 
 
 int main(){
